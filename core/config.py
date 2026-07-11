3caa69ef8ff245adbe28d2232f36670cf2b6c24a
skibidi <b91175533@gmail.com>
2026-07-11 16:17:30 +0530

    rdp commit

--- core/config.py
+++ core/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 WEB_SERVER_HOST = os.getenv("WEB_SERVER_HOST", "0.0.0.0")
 WEB_SERVER_PORT = int(os.getenv("WEB_SERVER_PORT", 3002))
 BASE_WEBHOOK_URL = os.getenv("BASE_WEBHOOK_URL", "https://reqbot.telesmm.in")
-OTHER_BOTS_PATH = os.getenv("OTHER_BOTS_PATH", "/webhook/{bot_token}")
+OTHER_BOTS_PATH = os.getenv("OTHER_BOTS_PATH", f"/webhook/{{bot_token}}")
+
 
 active_bots = {}
